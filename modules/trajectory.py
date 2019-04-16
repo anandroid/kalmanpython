e1e8e02232ba69bd3364c0213e866abde92008c4
Manthan Bharat Bhatt <manthan@Manthans-MacBook-Pro.local>
2019-04-16 10:10:58 -0700

    small changes in trajectory file

--- modules/trajectory.py
+++ modules/trajectory.py
@@ -1,10 +1,6 @@
 class Trajectory:
 
-	def __init__(self, from_point, to_point, init_theta):
-		
-		self.from_point = from_point
-		self.to_point = to_point
-		self.init_theta = init_theta
+	def __init__(self):
 
 		self.turn_actions = ["turnCW", "turnCCW"]
 		self.move_actions = ["moveF"]
@@ -20,8 +16,33 @@ class Trajectory:
 	def _map_to_action(self):
 		pass
 
-	def check_correction(self, current_point):
+	def check_correction(self, from_point, to_point, current_point):
 		pass
 
-	def plan(self):
-		pass
+	def plan(self, from_point, to_point, theta):
+
+		# values of theta
+		# 	0 - facing north
+		# 	90 - facing east
+		# 	180 - facing south
+		# 	270 - facing west
+		# use math.degrees to convert from radian to degrees. example on line number 13
+
+		actions = []
+
+		delta_x = self.to_point[0] - self.from_point[0]
+		delta_y = self.from_point[1] - self.to_point[1]
+
+		# case for moving towards north
+		if delta_x == 0 and delta_y > 0:
+		
+		# case for moving towards south
+		elif delta_x == 0 and delta_y < 0:
+
+		# case for moving towards east
+		elif delta_y == 0 and delta_x > 0:
+
+		# case for moving towards west
+		elif delta_y == 0 and delta_x < 0:
+
+
